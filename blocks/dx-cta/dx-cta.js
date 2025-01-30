dd4c2ad57516670162fc75ac54829d2584524327
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2025-01-31 01:39:03 +0530

    Update dx-cta.js

--- blocks/dx-cta/dx-cta.js
+++ blocks/dx-cta/dx-cta.js
@@ -9,8 +9,7 @@ containerDiv.classList.add('cta-wrapper--row','cta-wrapper--center');
 mainDiv.appendChild(containerDiv);
 
 var endingAnchor = document.createElement('a');
-var insightContentOne = document.querySelector('.dx-cta-wrapper > .dx-cta > div > div > p');
-var insightContentlink = document.querySelector('.dx-cta-wrapper > .dx-cta > div > div > p');
+var insightContentlink = document.querySelectorAll('.dx-cta > div > div > p')[1].innerText
 endingAnchor.setAttribute('id','cta-1-533502-508572');
 endingAnchor.setAttribute('class','btn');
 endingAnchor.classList.add('btn--primary','btn--medium');
@@ -19,5 +18,7 @@ endingAnchor.setAttribute('title','Enquire about a StarTrack account');
 endingAnchor.setAttribute('data-event','site interaction');
 endingAnchor.setAttribute('data-category','|btn');
 endingAnchor.setAttribute('data-description','enquire-about-a-startrack-account');
+var insightContentOne = document.querySelectorAll('.dx-cta > div > div > p')[0].innerText
+//var insightContentOne = document.querySelector('.dx-cta-wrapper > .dx-cta > div > div > p');
 endingAnchor.appendChild(insightContentOne);
 containerDiv.appendChild(endingAnchor);
