f3d72d334cb73dd214e346fe022d1b4d7008ff0b
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2025-01-31 01:13:47 +0530

    Create dx-cta.js

---
+++ blocks/dx-cta/dx-cta.js
@@ -0,0 +1,22 @@
+//Getting main Div container
+var mainDiv = document.querySelector('.dx-cta');
+
+//creating div
+var containerDiv = document.createElement('div');
+//adding class to container Div
+containerDiv.setAttribute('class','cta-wrapper');
+containerDiv.classList.add('cta-wrapper--row','cta-wrapper--center');
+mainDiv.appendChild(containerDiv);
+
+var endingAnchor = document.createElement('a');
+ var insightContentOne = document.querySelector('.dx-cta').innerText;
+ endingAnchor.setAttribute('id','cta-1-533502-508572');
+ endingAnchor.setAttribute('class','btn');
+endingAnchor.classList.add('btn--primary','btn--medium');
+  endingAnchor.setAttribute('href','/sales-enquiry');
+  endingAnchor.setAttribute('title',insightContentOne);
+    endingAnchor.setAttribute('data-event','site interaction');
+endingAnchor.setAttribute('data-category','|btn');
+endingAnchor.setAttribute('data-description','enquire-about-a-startrack-account');
+    endingAnchor.innerHTML = insightContentOne;
+  containerDiv.appendChild(endingAnchor);
