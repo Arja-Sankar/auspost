19d7f8ccf57c17323944956f6019d246bfcd8466
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2025-01-31 01:47:14 +0530

    Update dx-cta.js

--- blocks/dx-cta/dx-cta.js
+++ blocks/dx-cta/dx-cta.js
@@ -9,16 +9,16 @@ containerDiv.classList.add('cta-wrapper--row','cta-wrapper--center');
 mainDiv.appendChild(containerDiv);
 
 var endingAnchor = document.createElement('a');
-let insightContentOne = document.querySelectorAll('.dx-cta > div > div > p')[0].innerText
-let insightContentlink = document.querySelectorAll('.dx-cta > div > div > p')[1].innerText
+//let insightContentOne = document.querySelectorAll('.dx-cta > div > div > p')[0].innerText
+//let insightContentlink = document.querySelectorAll('.dx-cta > div > div > p')[1].innerText
 endingAnchor.setAttribute('id','cta-1-533502-508572');
 endingAnchor.setAttribute('class','btn');
 endingAnchor.classList.add('btn--primary','btn--medium');
-endingAnchor.setAttribute('href',insightContentlink);
+//endingAnchor.setAttribute('href',insightContentlink);
 endingAnchor.setAttribute('title','Enquire about a StarTrack account');
 endingAnchor.setAttribute('data-event','site interaction');
 endingAnchor.setAttribute('data-category','|btn');
 endingAnchor.setAttribute('data-description','enquire-about-a-startrack-account');
 //var insightContentOne = document.querySelector('.dx-cta-wrapper > .dx-cta > div > div > p');
-endingAnchor.appendChild(insightContentOne);
+//endingAnchor.appendChild(insightContentOne);
 containerDiv.appendChild(endingAnchor);
