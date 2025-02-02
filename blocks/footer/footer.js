31820281b42bc96a12139e459483045bcbcda47d
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2025-02-02 11:58:34 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -108,9 +108,10 @@ footDivone.appendChild(footLinksDiv);
     var footcpyrightDiv = document.createElement('div');
   footcpyrightDiv.setAttribute('class','copyright');
 footDivone.appendChild(footcpyrightDiv);
- footcpyrightDiv.innerHTML = document.querySelectorAll('.default-content-wrapper > p')[4].innerText;
+ footcpyrightDiv.innerHTML = document.querySelectorAll('.default-content-wrapper > p')[2].textContent;
 
   document.querySelectorAll('.default-content-wrapper > p')[3].style.display = 'none';
    document.querySelectorAll('.default-content-wrapper > p')[4].style.display = 'none';
+  document.querySelectorAll('.default-content-wrapper > p')[2].style.display = 'none';
  
 }
