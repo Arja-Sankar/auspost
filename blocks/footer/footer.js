24c5b8808fc8503f1b4b58869cdada0d99dcedbd
Arja-Sankar <139428000+Arja-Sankar@users.noreply.github.com>
2025-02-02 12:35:55 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -114,24 +114,7 @@ document.querySelector('.dx-copyright > div > div > p').style.display = 'none';
   var footanchor = document.createElement('div');
   footanchor.setAttribute('class','social');
   footDivone.appendChild(footanchor);
-    var footlasttDiv = document.createElement('div');
-  footlasttDiv.setAttribute('class','atsifooter__container');
-  footlastDiv.appendChild(footlasttDiv);
-
-  var footImg = document.createElement('img');
-  var Imgsrc = document.querySelector('.dx-footendimg > div > div > picture > img').src;
-  footImg.setAttribute('src,Imgsrc);
-   footImg.setAttribute('alt','');
-   footImg.setAttribute('aria-hidden','true');
-   footImg.setAttribute('tabindex','1');
-  footlasttDiv.appendChild(footImg);
-  // document.querySelector('.dx-footendimg > div > div').style.display = 'none';
-
-    footanchor.innerHTML = `  <a class="gf-social-links-icon" href="https://www.linkedin.com/company/australia-post" target="_blank" aria-label="Australia Post linkedin" data-event="site interaction" data-description="linkedin" data-platform="linkedin">
-                <span class="icon">
-                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" focusable="false" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.2 5.6a1.6 1.6 0 11-3.201-.001A1.6 1.6 0 017.2 5.6zM20 13.914c0-2.988-.483-5.296-3.973-5.296C13.5 8.618 12 9.845 12 10.5V8.8H8.8V20H12v-5.49c0-1.446.744-2.85 2.538-2.85 1.766 0 2.262 1.657 2.262 2.943V20H20v-6.086zM4 20h3.2V8.8H4V20z"></path></svg>
-                </span>
-            </a>`;
+  
 var footlastDiv = document.createElement('div');
 footlastDiv.setAttribute('class','atsifooter');
    mainFooter.appendChild(footlastDiv);
